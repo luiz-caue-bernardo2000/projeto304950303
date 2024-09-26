9107dba9d10d29a8c73d746581d9c1de153d1e16
luiz-caue-bernardo2000 <161481188+luiz-caue-bernardo2000@users.noreply.github.com>
2024-09-26 11:34:18 -0300

    main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 alert('caue e bernardo, numero 7 e 8')
 function soma(num1,num2){
     return num1+num2;
-
+}
+function areaquadrado(lado1,lado2){
+    return lado1*lado2;
 }
 console.log(soma(5,5));
+console.log(areaquadrado(25,25));
