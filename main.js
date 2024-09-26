556a64b89f6654fd629c1a85cb8e561e13b5ecfa
luiz-caue-bernardo2000 <161481188+luiz-caue-bernardo2000@users.noreply.github.com>
2024-09-26 12:25:22 -0300

    main.js

--- main.js
+++ main.js
@@ -6,3 +6,14 @@ function areaquadrado(lado1){
     return lado1*lado1;
 }
 console.log(areaquadrado(area));
+
+let valoresDaMedia1=parseInt (prompt('valores da media'))
+let valoresDaMedia2=parseInt (prompt('valores da media'))
+let valoresDaMedia3=parseInt (prompt('valores da media'))
+document.write(valoresDaMedia1)
+document.write(valoresDaMedia2)
+document.write(valoresDaMedia3);
+function media(valor1,valor2,valor3){
+  return (valor1+valor2+valor3)/3;
+}
+console.log(media(valoresDaMedia1, valoresDaMedia2, valoresDaMedia3));
