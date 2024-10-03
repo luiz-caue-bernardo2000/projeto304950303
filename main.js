1140ff14d0cc8cf5e51c1af4bf73f92ccc3f6597
luiz-caue-bernardo2000 <161481188+luiz-caue-bernardo2000@users.noreply.github.com>
2024-10-03 11:40:16 -0300

    main.js

--- main.js
+++ main.js
@@ -18,4 +18,4 @@
 // }
 // console.log(media(valoresDaMedia1, valoresDaMedia2, valoresDaMedia3));
 
-let estados = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SE','TO']
+let estados = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO']
