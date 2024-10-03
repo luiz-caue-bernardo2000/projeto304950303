fce141c546ad49eac18c7da01c8b607a3b732002
luiz-caue-bernardo2000 <161481188+luiz-caue-bernardo2000@users.noreply.github.com>
2024-10-03 11:34:18 -0300

    main.jsmain.js

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-alert('caue e bernardo, numero 7 e 8')
-let area=parseInt (prompt('area'))
-document.write(area);
+// alert('caue e bernardo, numero 7 e 8')
+// let area=parseInt (prompt('area'))
+// document.write(area);
 
-function areaquadrado(lado1){
-    return lado1*lado1;
-}
-console.log(areaquadrado(area));
+// function areaquadrado(lado1){
+//     return lado1*lado1;
+// }
+// console.log(areaquadrado(area));
 
-let valoresDaMedia1=parseInt (prompt('valores da media'))
-let valoresDaMedia2=parseInt (prompt('valores da media'))
-let valoresDaMedia3=parseInt (prompt('valores da media'))
-document.write(valoresDaMedia1)
-document.write(valoresDaMedia2)
-document.write(valoresDaMedia3);
-function media(valor1,valor2,valor3){
-  return (valor1+valor2+valor3)/3;
-}
-console.log(media(valoresDaMedia1, valoresDaMedia2, valoresDaMedia3));
+// let valoresDaMedia1=parseInt (prompt('valores da media'))
+// let valoresDaMedia2=parseInt (prompt('valores da media'))
+// let valoresDaMedia3=parseInt (prompt('valores da media'))
+// document.write(valoresDaMedia1)
+// document.write(valoresDaMedia2)
+// document.write(valoresDaMedia3);
+// function media(valor1,valor2,valor3){
+//   return (valor1+valor2+valor3)/3;
+// }
+// console.log(media(valoresDaMedia1, valoresDaMedia2, valoresDaMedia3));
 
 let estados = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SE','TO']
-console.log(estados.27)
